c33d618f37319ec5772921cafc47c7443fee8e8b
NairVillegas <u22205115@utp.edu.pe>
2024-11-25 22:13:45 -0500

    Delete src directory

--- src/main/java/com/flamabrava/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flamabrava.repository;
-
-import com.flamabrava.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
